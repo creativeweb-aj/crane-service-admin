75a881fc755d82515284b5acce4afc12d47a1d46
Ajay kumar sharma <48996332+creativeweb-aj@users.noreply.github.com>
2023-08-28 20:38:48 +0530

    update pwa removed

--- DjangoBaseSetup/configuration/config.py
+++ DjangoBaseSetup/configuration/config.py
@@ -41,38 +41,38 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=90),
 }
 
-PWA_APP_NAME = 'ADOT Admin'
-PWA_APP_DESCRIPTION = "TURNING IDEAS INTO INNOVATIVE SOFTWARE SOLUTIONS."
-PWA_APP_THEME_COLOR = '#000000'
-PWA_APP_BACKGROUND_COLOR = '#FFFFFF'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_STATUS_BAR_COLOR = 'default'
-PWA_APP_ICONS = [
-    {
-        # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/icon-160x160.png',
-        'src': '/static/img/icon-160x160.png',
-        'sizes': '160x160'
-    }
-]
-PWA_APP_ICONS_APPLE = [
-    {
-        # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/icon-160x160.png',
-        'src': '/static/img/icon-160x160.png',
-        'sizes': '160x160'
-    }
-]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/ADOT.svg',
-        'src': '/static/img/ADOT.svg',
-        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-    }
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
+# PWA_APP_NAME = 'ADOT Admin'
+# PWA_APP_DESCRIPTION = "TURNING IDEAS INTO INNOVATIVE SOFTWARE SOLUTIONS."
+# PWA_APP_THEME_COLOR = '#000000'
+# PWA_APP_BACKGROUND_COLOR = '#FFFFFF'
+# PWA_APP_DISPLAY = 'standalone'
+# PWA_APP_SCOPE = '/'
+# PWA_APP_ORIENTATION = 'any'
+# PWA_APP_START_URL = '/'
+# PWA_APP_STATUS_BAR_COLOR = 'default'
+# PWA_APP_ICONS = [
+#     {
+#         # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/icon-160x160.png',
+#         'src': '/static/img/icon-160x160.png',
+#         'sizes': '160x160'
+#     }
+# ]
+# PWA_APP_ICONS_APPLE = [
+#     {
+#         # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/icon-160x160.png',
+#         'src': '/static/img/icon-160x160.png',
+#         'sizes': '160x160'
+#     }
+# ]
+# PWA_APP_SPLASH_SCREEN = [
+#     {
+#         # 'src': 'https://mahalaxmi-crane-service.s3.amazonaws.com/static/img/ADOT.svg',
+#         'src': '/static/img/ADOT.svg',
+#         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+#     }
+# ]
+# PWA_APP_DIR = 'ltr'
+# PWA_APP_LANG = 'en-US'
 
 # AWS S3 Bucket Configurations
 # AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
